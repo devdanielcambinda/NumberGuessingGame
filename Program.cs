ee6a4e5b9ffc2db434319eb65aecc2737983b9e1
danielcambinda190100190 <190100190@esg.ipsantarem.pt>
2020-07-20 20:39:13 +0100

    menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Number Guessing Game");
+
+            int option, number = 0, guess, tries, tip;
+            char right;
+
+            do
+            {
+                Console.WriteLine("******************************************");
+                Console.WriteLine("*           Number Guessing Game         *");
+                Console.WriteLine("*  1- Player tries to guess the number   *");
+                Console.WriteLine("*  2- Computer tries to guess the number *");
+                Console.WriteLine("*  3- History                            *");
+                Console.WriteLine("*  0- Quit                               *");
+                Console.WriteLine("******************************************");
+                option = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{option}");
+
+                Console.ReadKey();
+
+            } while (option != 0);
+
 
         }
     }
