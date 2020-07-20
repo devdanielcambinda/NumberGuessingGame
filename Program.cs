ee6dcc6bf2f12852da597b72c8588b7fb5d01513
danielcambinda190100190 <190100190@esg.ipsantarem.pt>
2020-07-20 21:17:19 +0100

    option 1 added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Guessing_Game
         static void Main(string[] args)
         {
 
-            int option, number = 0, guess, tries, tip;
+            int option, number , guess, tries, tip;
             char right;
 
             do
@@ -21,9 +21,44 @@ namespace Guessing_Game
                 Console.WriteLine("******************************************");
                 option = Int32.Parse(Console.ReadLine());
 
-                Console.WriteLine($"{option}");
-
-                Console.ReadKey();
+                switch (option)
+                {
+                    case 1:
+                        Random randomNumber = new Random();
+                        number = randomNumber.Next(0, 101);
+                        tries = 0;
+                        do
+                        {
+                            Console.WriteLine("Try to guess the number (0-100)");
+                            guess = Convert.ToInt32(Console.ReadLine());
+                            tries++;
+                            if (guess > number)
+                            {
+                                Console.WriteLine($"The number is smaller than {guess}.\n");
+                            }else if(guess < number )
+                            {
+                                Console.WriteLine($"The number is bigger than {guess}.\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Correct ! You guessed the number {number} in {tries} try/tries .\n ");
+                            }
+                        } while (number != guess);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Please insert a valid input!");
+                        Console.WriteLine("Press any key...");
+                        Console.ReadKey();
+                        break;
+                        
+                }
+                
 
             } while (option != 0);
 
